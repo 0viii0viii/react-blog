c45361507b5a80292a6aa806fc1dcfcb3f58505e
0viii0viii <ffff00yllw@gmail.com>
2020-12-30 23:48:28 +0900

    feat router

--- server/app.js
+++ server/app.js
@@ -1,7 +1,42 @@
 import express from 'express';
+import hpp from 'hpp';
+import mongoose from 'mongoose';
+import config from './config';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+
+// Routes
+import postRoutes from './routes/api/post';
+import userRoutes from './routes/api/user';
+
 
 const app = express()
+const { MONGO_URI } = config;
 
-app.get('/')
+//security in deployment
+app.use(hpp());
+app.use(helmet());
+app.use(cors({origin:true, credentials: true}));
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+
+mongoose
+    .connect(MONGO_URI, {
+        
+            useNewUrlParser:true,
+            useUnifiedTopology: true,
+            useCreateIndex: true, 
+    
+    })
+    .then(()=> console.log("MongoDB is successfully connected!"))
+    .catch((e)=> console.log(e));
+
+// Use routes
+app.get('/');
+app.use('/api/post', postRoutes);
+app.use('/api/user', userRoutes);
 
 export default app;
